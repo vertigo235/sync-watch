c4234db384346147a46e503ea70fdb13a250dfc3
vertigo235 <jbjonesclt@gmail.com>
2017-12-29 16:10:25 -0500

    Update config

--- syncwatch/utils/config.py
+++ syncwatch/utils/config.py
@@ -1,8 +1,14 @@
+import os
 from decouple import config
 
-DEBUG = config('DEBUG', True, cast=bool)
+DEBUG = config('DEBUG', False, cast=bool)
 SERVER_URL = config('SERVER_URL')
 SERVER_TOKEN = config('SERVER_TOKEN')
 SERVER_NAME = config('SERVER_NAME')
-CHECK_INTERVAL = config('CHECK_INTERVAL', 60, cast=int)
-LOGFILE = config('LOGFILE', '/config/status.log')
+CHECK_INTERVAL = config('CHECK_INTERVAL', 5, cast=int)
+if os.environ.get('DOCKER') == 'YES':
+    LOGFILE = config('LOGFILE', '/config/status.log')
+else:
+    LOGFILE = config('LOGFILE', os.path.join( os.path.dirname(os.path.dirname(__file__)), 'status.log'))
+
+
