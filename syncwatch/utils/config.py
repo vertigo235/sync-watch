d5da8a85d2b51d35359702373765aedde86e088b
vertigo235 <jbjonesclt@gmail.com>
2020-01-01 19:01:03 -0500

    Add BW Paramaters to config

--- syncwatch/utils/config.py
+++ syncwatch/utils/config.py
@@ -6,6 +6,9 @@ SERVER_URL = config('SERVER_URL')
 SERVER_TOKEN = config('SERVER_TOKEN')
 SERVER_NAME = config('SERVER_NAME')
 CHECK_INTERVAL = config('CHECK_INTERVAL', 5, cast=int)
+BW_MAX = config('BW_MAX', 1750, cast=int)
+BW_FLOOR = config('BW_FLOOR', 100, cast=int)
+BW_FACTOR = config('BW_FACTOR', 1.1, cast=float)
 if os.environ.get('DOCKER') == 'YES':
     LOGFILE = config('LOGFILE', '/config/status.log')
 else:
